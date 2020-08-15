3237ac209631c29d024b4c618868acf2abd16358
scottms724 <scottms724@gmail.com>
2020-08-15 14:46:24 -0400

    add serializers

--- app/controllers/api/v1/combinations_controller.rb
+++ app/controllers/api/v1/combinations_controller.rb
@@ -2,7 +2,8 @@ class Api::V1::CombinationsController < ApplicationController
 
     def index 
         combinations = Combination.all 
-        render json: combinations  
+        #render json: combinations  
+        render json: CombinationSerializer.new(combinations)
     end
 
 end
